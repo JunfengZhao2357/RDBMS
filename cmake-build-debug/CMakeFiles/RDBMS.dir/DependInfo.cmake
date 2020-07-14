1112f3c6a92b091b5f287f479aafb1d24296038d
Junfeng Zhao <z15510083015@gmail.com>
2020-07-13 23:00:11 -0700

    add cmakelist.txt

---
+++ cmake-build-debug/CMakeFiles/RDBMS.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/zhaojunfeng/RDBMS/RDBMS/AppProcessor.cpp" "/Users/zhaojunfeng/RDBMS/RDBMS/cmake-build-debug/CMakeFiles/RDBMS.dir/AppProcessor.cpp.o"
+  "/Users/zhaojunfeng/RDBMS/RDBMS/Attribute.cpp" "/Users/zhaojunfeng/RDBMS/RDBMS/cmake-build-debug/CMakeFiles/RDBMS.dir/Attribute.cpp.o"
+  "/Users/zhaojunfeng/RDBMS/RDBMS/CommandProcessor.cpp" "/Users/zhaojunfeng/RDBMS/RDBMS/cmake-build-debug/CMakeFiles/RDBMS.dir/CommandProcessor.cpp.o"
+  "/Users/zhaojunfeng/RDBMS/RDBMS/DBProcessor.cpp" "/Users/zhaojunfeng/RDBMS/RDBMS/cmake-build-debug/CMakeFiles/RDBMS.dir/DBProcessor.cpp.o"
+  "/Users/zhaojunfeng/RDBMS/RDBMS/Database.cpp" "/Users/zhaojunfeng/RDBMS/RDBMS/cmake-build-debug/CMakeFiles/RDBMS.dir/Database.cpp.o"
+  "/Users/zhaojunfeng/RDBMS/RDBMS/Filters.cpp" "/Users/zhaojunfeng/RDBMS/RDBMS/cmake-build-debug/CMakeFiles/RDBMS.dir/Filters.cpp.o"
+  "/Users/zhaojunfeng/RDBMS/RDBMS/Index.cpp" "/Users/zhaojunfeng/RDBMS/RDBMS/cmake-build-debug/CMakeFiles/RDBMS.dir/Index.cpp.o"
+  "/Users/zhaojunfeng/RDBMS/RDBMS/Row.cpp" "/Users/zhaojunfeng/RDBMS/RDBMS/cmake-build-debug/CMakeFiles/RDBMS.dir/Row.cpp.o"
+  "/Users/zhaojunfeng/RDBMS/RDBMS/SQLProcessor.cpp" "/Users/zhaojunfeng/RDBMS/RDBMS/cmake-build-debug/CMakeFiles/RDBMS.dir/SQLProcessor.cpp.o"
+  "/Users/zhaojunfeng/RDBMS/RDBMS/Schema.cpp" "/Users/zhaojunfeng/RDBMS/RDBMS/cmake-build-debug/CMakeFiles/RDBMS.dir/Schema.cpp.o"
+  "/Users/zhaojunfeng/RDBMS/RDBMS/Statement.cpp" "/Users/zhaojunfeng/RDBMS/RDBMS/cmake-build-debug/CMakeFiles/RDBMS.dir/Statement.cpp.o"
+  "/Users/zhaojunfeng/RDBMS/RDBMS/Storage.cpp" "/Users/zhaojunfeng/RDBMS/RDBMS/cmake-build-debug/CMakeFiles/RDBMS.dir/Storage.cpp.o"
+  "/Users/zhaojunfeng/RDBMS/RDBMS/StorageBlock.cpp" "/Users/zhaojunfeng/RDBMS/RDBMS/cmake-build-debug/CMakeFiles/RDBMS.dir/StorageBlock.cpp.o"
+  "/Users/zhaojunfeng/RDBMS/RDBMS/Tokenizer.cpp" "/Users/zhaojunfeng/RDBMS/RDBMS/cmake-build-debug/CMakeFiles/RDBMS.dir/Tokenizer.cpp.o"
+  "/Users/zhaojunfeng/RDBMS/RDBMS/main.cpp" "/Users/zhaojunfeng/RDBMS/RDBMS/cmake-build-debug/CMakeFiles/RDBMS.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/RDBMS.dir/cmake_clean.cmake
@@ -0,0 +1,24 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RDBMS.dir/AppProcessor.cpp.o"
+  "CMakeFiles/RDBMS.dir/Attribute.cpp.o"
+  "CMakeFiles/RDBMS.dir/CommandProcessor.cpp.o"
+  "CMakeFiles/RDBMS.dir/DBProcessor.cpp.o"
+  "CMakeFiles/RDBMS.dir/Database.cpp.o"
+  "CMakeFiles/RDBMS.dir/Filters.cpp.o"
+  "CMakeFiles/RDBMS.dir/Index.cpp.o"
+  "CMakeFiles/RDBMS.dir/Row.cpp.o"
+  "CMakeFiles/RDBMS.dir/SQLProcessor.cpp.o"
+  "CMakeFiles/RDBMS.dir/Schema.cpp.o"
+  "CMakeFiles/RDBMS.dir/Statement.cpp.o"
+  "CMakeFiles/RDBMS.dir/Storage.cpp.o"
+  "CMakeFiles/RDBMS.dir/StorageBlock.cpp.o"
+  "CMakeFiles/RDBMS.dir/Tokenizer.cpp.o"
+  "CMakeFiles/RDBMS.dir/main.cpp.o"
+  "RDBMS"
+  "RDBMS.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/RDBMS.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
